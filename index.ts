a6da0856e97a892dc7e04bcb83c29cf93f532228
Balli Asghar <ballihacker@outlook.com>
2023-09-06 21:37:00 +0100

    Added Comments

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+// Import the necessary types from the aws-lambda package
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from 'aws-lambda';
 
-const handler = async (
+// Define the handler function with async/await syntax
+const handler: Handler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
+  // Return a response object with a 200 status code and a JSON body
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -11,4 +18,5 @@ const handler = async (
   };
 };
 
+// Export the handler function for use in AWS Lambda
 export { handler };
